0b4c2c45c8402199bafdef86598995363eceddff
LuisChiej <54555805+LuisChiej@users.noreply.github.com>
2023-02-27 10:52:51 +0100

    fix: `ReferenceError of btoa`

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,3 +1,7 @@
+const btoa = (text: string) => {
+    return Buffer.from(text, 'binary').toString('base64');
+};
+
 const base64 = (password: string, username: string): string => {
     const date = Math.round(Date.now()/1000);
     return btoa(`${password}|${username}|${date.toString()}`);
